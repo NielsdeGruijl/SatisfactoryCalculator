395461ad20f5f83f4f64b35d6e4db461b6974d8e
Niels De Gruijl <niels07@outlook.com>
2025-06-10 22:48:56 +0200

    v1.0 finished

--- Satisfactory_Calculator/Assets/Scripts/Settings.cs
+++ Satisfactory_Calculator/Assets/Scripts/Settings.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
@@ -9,15 +11,56 @@ public class Settings : MonoBehaviour
     public GameObject settingsMenu;
 
     public TMP_InputField cameraSensitivityInputField;
+    public TMP_Dropdown resolutionDropdown;
+    public Toggle fullscreenToggle;
 
+    private Resolution[] resolutions;
+    private List<Resolution> filteredResolutions = new List<Resolution>();
+    
     private void Start()
     {
+        //Screen.fullScreen = true;
         settingsMenu.SetActive(false);
+
+        resolutions = Screen.resolutions;
+        Screen.SetResolution(2560, 1440, true);
+        SetResolutionsDropDown();
     }
 
+    private void SetResolutionsDropDown()
+    {
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            Resolution resolution = resolutions[i];
+            if((float)resolution.height / ((float)resolution.width / 16) == 9)
+                filteredResolutions.Add(resolution);
+            
+            Debug.Log(resolution.height / (resolution.width / 16));
+        }
+
+        int currentResolutionIndex = 0;
+        List<String> resolutionOptions = new List<String>();
+        for (int i = 0; i < filteredResolutions.Count; i++)
+        {
+            Resolution resolution = filteredResolutions[i];
+            string resolutionOption = resolution.width + "x" + resolution.height;
+            resolutionOptions.Add(resolutionOption);
+
+            if (resolution.width == Screen.width && resolution.height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        
+        resolutionDropdown.AddOptions(resolutionOptions);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    
     public void OnCenterCamera()
     {
         Camera.main.transform.position = new Vector3(0, 0, -10);
+        Camera.main.orthographicSize = 5;
     }
 
     public void OnChangeCameraSensitivity()
@@ -29,6 +72,17 @@ public class Settings : MonoBehaviour
             return;
         }
         cameraSensitivity = float.Parse(cameraSensitivityInputField.text);
+    }
+
+    public void OnChangeResolution(int index)
+    {
+        Resolution resolution = filteredResolutions[resolutionDropdown.value];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreenMode);
+    }
+
+    public void OnChangeFullscreenMode(bool value)
+    {
+        Screen.fullScreen = fullscreenToggle.isOn;
     }
     
     public void OnOpenSettingsMenu()
